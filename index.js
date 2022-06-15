982786122fb5ef200cd68b72c52c1189fa52671c
Soumya Banerjee <soumyabanerjee783@gmail.com>
2022-06-15 12:35:23 +0530

    set up routes, created models

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import express from 'express'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+import postRoutes from './routes/posts.js'
+
+const app = express()
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+app.use('/posts', postRoutes)
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message))
