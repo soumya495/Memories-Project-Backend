dd60dba33f02dd6229e31a221995e545db311f93
Soumya Banerjee <soumyabanerjee783@gmail.com>
2022-06-17 23:04:16 +0530

    Authentication

--- routes/posts.js
+++ routes/posts.js
@@ -6,12 +6,13 @@ import {
   updatePost,
   deletePost,
 } from '../controllers/posts.js'
+import auth from '../middleware/auth.js'
 
 const router = express.Router()
 
 router.get('/', getPosts)
-router.post('/', createPost)
-router.patch('/:id', updatePost)
-router.delete('/:id', deletePost)
+router.post('/', auth, createPost)
+router.patch('/:id', auth, updatePost)
+router.delete('/:id', auth, deletePost)
 
 export default router
